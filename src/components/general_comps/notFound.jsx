466c58575f0b54d08e9f5ebf0e16ee877610910b
Chaya Segal <chayale90@gmail.com>
2023-09-14 20:33:17 +0300

    order noFound comp

--- src/components/general_comps/notFound.jsx
+++ src/components/general_comps/notFound.jsx
@@ -3,8 +3,9 @@ import { Link } from 'react-router-dom'
 
 export default function NotFound() {
     return (
-        <div className='not-found-comp bg-light p-5 text-center'>not Found! please click to go to
-            <Link to="/"> home-Page</Link>
+        <div>
+            <h1 className='not-found-comp bg-light p-5 text-center'>page not found 404!</h1>
+            <Link to="/"> Back to home</Link>
         </div>
 
     )
