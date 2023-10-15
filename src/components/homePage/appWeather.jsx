8bd42f6d3dc9e4fcd7d67eae5e6a820fdb15de9a
Chaya Segal <chayale90@gmail.com>
2023-10-15 20:21:37 +0300

    add env and order imports

--- src/components/homePage/appWeather.jsx
+++ src/components/homePage/appWeather.jsx
@@ -1,9 +1,9 @@
 
 import { useEffect, useMemo, useState } from "react"
 import { useDispatch, useSelector } from "react-redux";
+import { IconButton } from '@mui/material';
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
-import { IconButton } from '@mui/material';
 // project imports
 import WeatherInput from "./weatherInput";
 import WeatherInfo from "./WeatherInfo";
@@ -18,14 +18,20 @@ const AppWeather = () => {
     const dispatch = useDispatch();
 
     //hook
-    const { autocompleteObj, currentWeather, daysArr,headlineWeek, fetchWeatherData, } = useWeatherData();
-   
+    const {
+        autocompleteObj,
+        currentWeather,
+        daysArr,
+        headlineWeek,
+        fetchWeatherData
+    } = useWeatherData();
+
     //favorites
     const [isLiked, setIsLiked] = useState(false);
     const { arrFavorites } = useSelector((myStore) => myStore.featuresSlice);
 
     //darkMode
-    const { darkMode,flagSeeMore } = useSelector(myStore => myStore.featuresSlice);
+    const { darkMode, flagSeeMore } = useSelector(myStore => myStore.featuresSlice);
     const modeBackground = useMemo(() => {
         if (darkMode)
             return theme.palette.darkMode.main
@@ -33,8 +39,8 @@ const AppWeather = () => {
     }, [darkMode]);
 
     useEffect(() => {
-        if(!flagSeeMore)
-        fetchWeatherData("Tel Aviv");
+        if (!flagSeeMore)
+            fetchWeatherData("Tel Aviv");
     }, []);
 
     const temperatureCelsius = (currentWeather.Temperature?.Metric?.Value) + "°" + (currentWeather.Temperature?.Metric?.Unit);
@@ -44,11 +50,11 @@ const AppWeather = () => {
         temperature: temperatureCelsius,
         description: currentWeather.WeatherText,
         locationKey: autocompleteObj.Key,
-        currentWeather:currentWeather,
-        autocompleteObj:autocompleteObj,
-        daysArr:daysArr
+        currentWeather: currentWeather,
+        autocompleteObj: autocompleteObj,
+        daysArr: daysArr
     }
-   
+
     const clickOnLike = () => {
         if (!isLiked) {
             dispatch(addNewItem({ val: favoriteObj }))
@@ -56,6 +62,7 @@ const AppWeather = () => {
         }
     }
 
+    console.log(arrFavorites);
     //check if the weather obj exist in favorite array and update the 'isLiked' state 
     useMemo(() => {
         let isCurrentlyLiked = false;
@@ -93,9 +100,9 @@ const AppWeather = () => {
                 {(autocompleteObj.Key) ?
                     <>
                         <WeatherInfo currentWeather={currentWeather} />
-                        <DaysList daysArr={daysArr} headlineWeek={headlineWeek}/>
+                        <DaysList daysArr={daysArr} headlineWeek={headlineWeek} />
                     </>
-                    : <LoadingComp/>
+                    : <LoadingComp />
                 }
 
             </div>
